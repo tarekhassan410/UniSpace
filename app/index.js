87cf7db04fb9c4fd0563def4dff5edb06fc2bbc9
tarekhassan410 <tarekhassan410@gmail.com>
2018-11-28 21:41:45 +0200

    Login form added to the page main app

--- app/index.js
+++ app/index.js
@@ -2,40 +2,64 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-
-  constructor(props){
-      super(props)
-      this.state = {
-        authenticated: false,
-        value: ''
-    }
-    this.logIn = this.logIn.bind(this)
-    this.Logout = this.Logout.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+      value: ""
+    };
+    this.logIn = this.logIn.bind(this);
+    this.Logout = this.Logout.bind(this);
   }
 
-  logIn(e){
-      this.setState({
-        authenticated: true,
-        value: 'Hello Tarek'
-      })
-  } 
+  logIn(e) {
+    this.setState({
+      authenticated: true,
+      value: "Hello Tarek"
+    });
+  }
 
-  Logout(e){
+  Logout(e) {
     this.setState({
       authenticated: false,
-      value: 'logged out'
-    })
+      value: "logged out"
+    });
   }
 
   render() {
     return (
       <div className="section has-text-centered">
-        {this.state.authenticated ?
-        <button onClick={ this.Logout} className="button is-warning"> Log out </button>
-        : 
-        <button onClick={ this.logIn} className="button is-large is-link"> Login to Google drive </button>
-        }
-        <p> {this.state.value} </p>
+        {this.state.authenticated ? (
+          <div className='box'>
+            <div className='field'>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="email" placeholder="Email" />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope" />
+                </span>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check" />
+                </span>
+              </div>
+            </div>
+            <div className='field'>
+              <p class="control has-icons-left">
+                <input class="input" type="password" placeholder="Password" />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-lock" />
+                </span>
+              </p>
+            </div>
+            <button className="button is-link">
+            Login
+          </button>
+          </div>
+        ) : (
+          <button onClick={this.logIn} className="button is-large is-link">
+            {" "}
+            Login to Google drive{" "}
+          </button>
+        )}
       </div>
     );
   }
